10e807f8b71efd892a5ec5210f4b13c604a7f05f
sandman <stone.zzz.linxz@gmail.com>
2014-03-27 23:27:46 +0800

    修改了请求顺序 在maven添加了log4j，移动了资源文件位置

--- robot/src/main/java/org/dawneye/xjrobot/service/GameClientInit.java
+++ robot/src/main/java/org/dawneye/xjrobot/service/GameClientInit.java
@@ -2,99 +2,177 @@ package org.dawneye.xjrobot.service;
 
 import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpState;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.log4j.Logger;
 import org.dawneye.xjrobot.util.TimeUtil;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by sandman on 14-3-12.
  */
 public class GameClientInit {
+    private static final Logger logger = Logger.getLogger(GameClientInit.class);
+    private HttpClient httpclient;
+
+    public GameClientInit(HttpClient httpclient) {
+        this.httpclient = httpclient;
+    }
+
     public static void main(String[] args) throws Exception {
-//        if (args.length != 1) {
-//            System.err.println("Usage: java CookieDemoApp <url>");
-//            System.err.println("<url> The url of a webpage");
-//            System.exit(1);
-//        }
-        GameClientInit gci = new GameClientInit();
-        List<String> urls = new ArrayList<String>();
-        urls.add("http://wan.xianjian.com/client/Application/versionInfo.txt?v=" + TimeUtil.getJsonpSuffix());
-        urls.add("http://www.xianjian.com/client?=" + TimeUtil.getJsonpSuffix() + "&channel=xinxianjian_v1.12.exe");
-        urls.add("http://register.xianjian.com/lasted-json?cb=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix());
-        urls.add("http://register.xianjian.com/plugins/xjlogin?screenName=dakeshuichong&password=qq361972238&channel=xinxianjian_v1.12.exe&callbackparam=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix());
-        urls.add("http://register.xianjian.com/lasted-json?cb=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix());
-        urls.add("http://www.xianjian.com/client/server.html");
-        urls.add("http://register.xianjian.com/lasted-json?cb=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix());
-        gci.request(urls);
+        HttpClient myHttpClient = new HttpClient();
+        myHttpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);//浏览器兼容模式
+        GameClientInit gci = new GameClientInit(myHttpClient);
+        gci.httpclient.getHttpConnectionManager().getParams().setConnectionTimeout(30000);//超时时间30秒(3000毫秒)
+
+        gci.request1();
+        gci.request2();
+        gci.request3();
+        gci.request4();
+        gci.request5();
+        gci.request6();
+        gci.request7();
     }
 
-    public void request(List<String> strURLs) throws IOException {
-        // Get target URL
-//        String strURL = args[0];
-//        String strURL = "http://wan.xianjian.com/client/Application/versionInfo.txt";
-        System.out.println("Target URL: " + strURLs);
-
-        // Get initial state object
-        HttpState initialState = new HttpState();
-        // Initial set of cookies can be retrieved from persistent storage and
-        // re-created, using a persistence mechanism of choice,
-//        Cookie mycookie = new Cookie("", "", "", "/", null, false);
-//        Cookie mycookie = new Cookie();
-        // and then added to your HTTP state instance
-//        initialState.addCookie(mycookie);
-
-        // Get HTTP client instance
-        HttpClient httpclient = new HttpClient();
-        httpclient.getHttpConnectionManager().
-                getParams().setConnectionTimeout(30000);
-        httpclient.setState(initialState);
-
-        // RFC 2101 cookie management spec is used per default
-        // to parse, validate, format & match cookies
-        httpclient.getParams().setCookiePolicy(CookiePolicy.RFC_2109);
-        // A different cookie management spec can be selected
-        // when desired
-        for(String strURL: strURLs){
-            requestAndPrint(strURL, httpclient);
-        }
-        //httpclient.getParams().setCookiePolicy(CookiePolicy.NETSCAPE);
-        // Netscape Cookie Draft spec is provided for completeness
-        // You would hardly want to use this spec in real life situations
-        //httppclient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
-        // Compatibility policy is provided in order to mimic cookie
-        // management of popular web browsers that is in some areas
-        // not 100% standards compliant
-
-//ss
-
-    }
-
-    public void requestAndPrint(String strURL, HttpClient httpclient) throws IOException {
-// Get HTTP GET method
+    public void request1() throws IOException {
+        //版本检查
+        String strURL = "http://wan.xianjian.com/client/Application/versionInfo.txt?v=" + TimeUtil.getJsonpSuffix();
+        logger.info("request url: " + strURL);
         GetMethod httpget = new GetMethod(strURL);
         httpget.setRequestHeader("","");
-        // Execute HTTP GET
-        int result = httpclient.executeMethod(httpget);
-        // Display status code
-        System.out.println("Response status code: " + result);
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
         if(HttpStatus.SC_OK == result){
             String response = httpget.getResponseBodyAsString();
-            System.out.println("Response context as text: " + response);
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
         }
-        // Get all the cookies+
-        Cookie[] cookies = httpclient.getState().getCookies();
-        // Display the cookies
-        System.out.println("Present cookies: ");
-        for (int i = 0; i < cookies.length; i++) {
-            System.out.println(" - " + cookies[i].toExternalForm());
+    }
+
+    public void request2() throws IOException {
+        //版本检查
+        String strURL = "http://www.xianjian.com/client?=" + TimeUtil.getJsonpSuffix() + "&channel=xinxianjian_v1.12.exe";
+        logger.info("request url: " + strURL);
+        GetMethod httpget = new GetMethod(strURL);
+        httpget.setRequestHeader("","");
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
+        if(HttpStatus.SC_OK == result){
+            String response = httpget.getResponseBodyAsString();
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
         }
-        // Release current connection to the connection pool once you are done
-//        httpget.releaseConnection();
+    }
+
+    public void request3() throws IOException {
+        //版本检查
+        String strURL = "http://register.xianjian.com/lasted-json?cb=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix();
+        logger.info("request url: " + strURL);
+        GetMethod httpget = new GetMethod(strURL);
+        httpget.setRequestHeader("","");
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
+        if(HttpStatus.SC_OK == result){
+            String response = httpget.getResponseBodyAsString();
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
+        }
+    }
+
+    public void request4() throws IOException {
+        //版本检查
+        String strURL = "http://register.xianjian.com/plugins/xjlogin?screenName=dakeshuichong&password=qq361972238&channel=xinxianjian_v1.12.exe&callbackparam=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix();
+        logger.info("request url: " + strURL);
+        GetMethod httpget = new GetMethod(strURL);
+        httpget.setRequestHeader("","");
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
+        if(HttpStatus.SC_OK == result){
+            String response = httpget.getResponseBodyAsString();
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
+        }
+    }
+
+    public void request5() throws IOException {
+        //版本检查
+        String strURL = "http://register.xianjian.com/lasted-json?cb=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix();
+        logger.info("request url: " + strURL);
+        GetMethod httpget = new GetMethod(strURL);
+        httpget.setRequestHeader("","");
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
+        if(HttpStatus.SC_OK == result){
+            String response = httpget.getResponseBodyAsString();
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
+        }
+    }
+
+    public void request6() throws IOException {
+        //版本检查
+        String strURL = "http://www.xianjian.com/client/server.html";
+        logger.info("request url: " + strURL);
+        GetMethod httpget = new GetMethod(strURL);
+        httpget.setRequestHeader("","");
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
+        if(HttpStatus.SC_OK == result){
+            String response = httpget.getResponseBodyAsString();
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
+        }
+    }
+
+    public void request7() throws IOException {
+        //版本检查
+        String strURL = "http://register.xianjian.com/lasted-json?cb=" + "jsonp" + TimeUtil.getJsonpTimeStampSuffix() + "&_=" + TimeUtil.getJsonpTimeStampLaterSuffix();
+        logger.info("request url: " + strURL);
+        GetMethod httpget = new GetMethod(strURL);
+        httpget.setRequestHeader("","");
+        logger.info("request parameters: " + httpget);
+        int result = this.httpclient.executeMethod(httpget);
+        logger.info("Response status code: " + result);
+        if(HttpStatus.SC_OK == result){
+            String response = httpget.getResponseBodyAsString();
+            logger.info("Response context as text: " + response);
+            Cookie[] cookies = this.httpclient.getState().getCookies();
+            logger.info("Present cookies: ");
+            for (int i = 0; i < cookies.length; i++) {
+                logger.info(" - " + cookies[i].toExternalForm());
+            }
+        }
     }
 }
